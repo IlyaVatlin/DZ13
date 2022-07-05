04cd4bfb8f81b898712e06113e93abac6559c7df
IlyaVatlin <107053785+IlyaVatlin@users.noreply.github.com>
2022-07-05 16:51:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int dengi = 100;
+        int popoln = 1100;
+        int bonus;
+
+        if (popoln > 1000) {
+            bonus = popoln / 100;
+        } else {
+            bonus = 0;
+        }
+
+        int balanc = dengi + popoln + bonus;
+        System.out.println("Ваш баланс: " + balanc + " руб. Ваш бонус: " + bonus + " руб.");
+    }
 }
